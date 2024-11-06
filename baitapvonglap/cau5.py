cba722468cbe5afa9ac9e6423fb4fd91bf7ccdb1
huongthongminhvcl <nthuong.24174600090@sv.uneti.edu.vn>
2024-11-06 19:00:09 +0700

    cau5.py

---
+++ baitapvonglap/cau5.py
@@ -0,0 +1,21 @@
+#Nhập số từ người dùng
+n = int(input("nhập một số nguyên:"))
+#Kiểm tra số hoàn hảo 
+if n < 1:
+    print(f"{n} không phải là số hoàn hảo.")
+else:
+    tong = 0 
+        #Khởi tạo biến tổng các ước số
+#Tính tổng các ước số dương của number
+for i in range(1,n):
+    if n % i == 0:
+        #Nếu i là số ước 
+        tong = tong + i 
+# so sánh tổng các ước số với số ban đầu 
+if tong == n:
+    print(f"{n}là số hoàn hảo.")
+else:
+    print(f"{n}không phải là số hoàn hảo.")
+
+
+    
